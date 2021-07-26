efc396acbbda34f6950f7fc368b38ad26a0a56e5
Eze Ogbonnaya <yexies4ogb@gmail.com>
2021-07-25 22:27:06 -0700

    yeah

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const mysql = require("mysql")
 var cors = require('cors')
 const bodyParser = require('body-parser')
